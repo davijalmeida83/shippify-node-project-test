cc391f079941c20e9a2ee178f957a2e5223cc905
Davi Almeida <davij_almeida@hotmail.com>
2026-03-23 11:46:10 -0300

    feat: adicionar threshold de cobertura mínima de 80% para validação

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,14 @@ module.exports = {
     '!**/node_modules/**',
   ],
   coverageReporters: ['text', 'lcov', 'html', 'json'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/dist/',
